5c7925a34dfa140dd40aa59509a77def8929c8f6
pratik8696 <mepratikanand@gmail.com>
2021-09-06 23:17:47 +0530

    initialised nextjs

---
+++ pages/news/[newsid].js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function newsdetails() {
+    return (
+        <div>
+            <h1>This is a dynamically loaded page</h1> 
+        </div>
+    )
+}
+
+export default newsdetails;
